5de4e7b8e108cbe66394133cee8a1cd1a2ae5b4d
nvtnicolas <nicolas.nguyenvanthanh@ynov.com>
2024-05-23 15:29:11 +0200

    Start to create the begining of adventure

---
+++ src/Chapters/GetPokemon.java
@@ -0,0 +1,13 @@
+package src.Chapters;
+import src.ClearTerminal;
+import src.TextDisplayer;
+public class GetPokemon {
+    public static void getPokemon(String[] args) {
+        String textToDisplay =
+                "You are now in front of 3 pokeballs. \n" +
+                "Each of them contains a Pokemon. \n" +
+                "You can only choose one of them. \n" +
+                "Choose wisely !";
+
+    }
+}
