8efc71ac592022dbaa9611c4faf205a2c332063c
ElCabrii <144121693+ElCabrii@users.noreply.github.com>
2024-05-30 15:06:35 +0200

    Ability class re-created

--- src/Pokemon/Ability.java
+++ src/Pokemon/Ability.java
@@ -2,8 +2,31 @@ package src.Pokemon;
 
 public class Ability {
     private String name;
-    private Type type;
+    private Type.ElementType type;
     private int power;
-    private int pp;
+    private int maxPp;
+    private int currentPp;
+    public Ability(String name, Type.ElementType type, int power, int maxPp) {
+        this.name = name;
+        this.type = type;
+        this.power = power;
+        this.maxPp = maxPp;
+        this.currentPp = maxPp;
+    }
+    public String getName() {
+        return name;
+    }
+    public Type.ElementType getType() {
+        return type;
+    }
+    public int getPower() {
+        return power;
+    }
+    public int getMaxPp() {
+        return maxPp;
+    }
+    public int getCurrentPp() {
+        return currentPp;
+    }
 }
 
