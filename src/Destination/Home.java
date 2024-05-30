069d716de24d646bff26335477fa5d6bf4426985
nvtnicolas <nicolas.nguyenvanthanh@ynov.com>
2024-05-30 16:29:29 +0200

    Change text

--- src/Destination/Home.java
+++ src/Destination/Home.java
@@ -1,7 +1,15 @@
 package src.Destination;
 
+import src.TextDisplayer;
+
 public class Home {
     public static void home(){
-        System.out.println("You are now at home");
+        String textToDisplay =
+                "You are now at home\n" +
+                "You can go to the laboratory or the park\n" +
+                "Enter the name of the destination you want to go to : ";
+
+
+        TextDisplayer.displayParagraph(textToDisplay);
     }
 }
