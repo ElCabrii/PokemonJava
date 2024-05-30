4cb89c1bd632eb9a4d5074b5b83b3f21382bea9d
ElCabrii <144121693+ElCabrii@users.noreply.github.com>
2024-05-30 15:08:01 +0200

    Class type recreated

---
+++ src/Pokemon/Type.java
@@ -0,0 +1,24 @@
+package src.Pokemon;
+
+public class Type {
+    public enum ElementType {
+        NORMAL,
+        FIRE,
+        WATER,
+        ELECTRIC,
+        GRASS,
+        ICE,
+        FIGHTING,
+        POISON,
+        GROUND,
+        FLYING,
+        PSYCHIC,
+        BUG,
+        ROCK,
+        GHOST,
+        DRAGON,
+        DARK,
+        STEEL,
+        FAIRY
+    }
+}
