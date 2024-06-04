a41add83b3f9ba66abdb2d6ea35544d442c0ee9a
ElCabrii <144121693+ElCabrii@users.noreply.github.com>
2024-06-04 14:06:52 +0200

    slight modifs

--- src/Pokemon/Ability.java
+++ src/Pokemon/Ability.java
@@ -2,21 +2,16 @@ package src.Pokemon;
 
 public class Ability {
     private String name;
-    private Type.ElementType type;
-    public Ability(String name, Type.ElementType type) {
+    private EnumTypes type;
+    public Ability(String name, EnumTypes type) {
         this.name = name;
         this.type = type;
     }
     public String getName() {
         return name;
     }
-    public Type.ElementType getType() {
+    public EnumTypes getType() {
         return type;
     }
-
-
-    public static Ability OVERGROW ;
-    public static Ability BLAZE ;
-    public static Ability TORRENT;
 }
 
