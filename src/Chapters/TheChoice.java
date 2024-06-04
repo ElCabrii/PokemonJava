998dd76cd2337f951ee32176b123586e2bdecf19
ElCabrii <144121693+ElCabrii@users.noreply.github.com>
2024-06-04 14:09:18 +0200

    asking which pokemon to choose

--- src/Chapters/TheChoice.java
+++ src/Chapters/TheChoice.java
@@ -1,26 +1,27 @@
 package src.Chapters;
 import src.Move;
 import src.TextDisplayer;
-import src.Player;
+import src.Trainer;
+
+import java.util.Scanner;
 
 
 public class TheChoice {
-    public static void theChoice(String[] args) {
-        String textToDisplay =
-                "Professor Java: Well done , you choose your first Pokemon ! \n" +
-                        "Now you need to give him a name ! \n" +
-                        //Create function to Name the Pokemon
+    public static Trainer theChoice(Trainer trainer) {
+        System.out.print("Choose your first Pokemon: ");
+        trainer.addPokemonToTeam(trainer.getStarterPokemon());
+        String[] textToDisplay =
+                {"Professor Java: Well done , you choose your first Pokemon ! \n" +
+                        "Now you need to give him a name ! \n",
                         "You are now ready to start your adventure ! \n" +
                         "But before that, you should go to your room and take your bag \n" +
-                        "And don't forget to say goodbye to your mom ! \n" ;
-
-        //Create function to Choose and add the Pokemon to the Player's team
-        Player.getStarterPokemon();
-
-        TextDisplayer.displayParagraph(textToDisplay);
-
-        String destination1 = "Home";
-        String destination2 = "Laboratory";
-        Move.move(destination1, destination2);
+                        "And don't forget to say goodbye to your mom ! \n"} ;
+        TextDisplayer.displayParagraph(textToDisplay[0]);
+        System.out.print("Enter a name for your Pokemon: ");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        trainer.getTeam()[0].setName(name);
+        TextDisplayer.displayParagraph(textToDisplay[1]);
+        return trainer;
     }
 }
