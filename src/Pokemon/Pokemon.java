463c7306b6e98e4c71ffcc10371fd6b67734ce6e
ElCabrii <144121693+ElCabrii@users.noreply.github.com>
2024-06-04 14:12:21 +0200

    some var added, method attack created

--- src/Pokemon/Pokemon.java
+++ src/Pokemon/Pokemon.java
@@ -1,52 +1,108 @@
 package src.Pokemon;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class Pokemon {
-        private String name;
-        private Type.ElementType[] types;
-        private Ability[] abilities;
-        private int maxHP;
-        private int currentHP;
+    private String name;
+    private EnumTypes[] types;
+    private List<Ability> abilities;
+    private int maxHP;
+    private int currentHP;
+    private int attack;
+    private int level;
+    private Status status;
+    private LivingStatus livingStatus;
+    public Pokemon(String name, EnumTypes[] types, List<Ability> abilities, int maxHP, int attack) {
+        this.name = name;
+        this.types = types;
+        this.abilities = abilities;
+        this.maxHP = maxHP;
+        this.currentHP = maxHP;
+        this.level = 1;
+        this.attack = attack;
+        this.status = Status.WILD;
+        this.livingStatus = LivingStatus.ALIVE;
+    }
 
+    public String getName() {
+        return name;
+    }
 
-        public Pokemon(String name, Type.ElementType[] types, Ability[] abilities, int maxHP) {
-            this.name = name;
-            this.types = types;
-            this.abilities = abilities;
-            this.maxHP = maxHP;
-            this.currentHP = maxHP;
-        }
-        public String getName() {
-            return name;
-        }
-        public Type.ElementType[] getTypes() {
-            return types;
-        }
-        public Ability[] getAbilities() {
-            return abilities;
-        }
-        public int getMaxHP() {
-            return maxHP;
-        }
-        public int getCurrentHP() {
-            return currentHP;
-        }
-        public void setCurrentHP(int currentHP) {
-            this.currentHP = currentHP;
-        }
-        public void catchPokemon() {
-            //To be implemented
-            System.out.println("You caught a " + name + "!");
-        }
-        public void attack(Pokemon target) {
-            //To be implemented
-            System.out.println("What ability would you like to use?");
-            for (int i = 0; i < abilities.length; i++) {
-                System.out.println(i + 1 + ". " + abilities[i].getName());
+    public EnumTypes[] getTypes() {
+        return types;
+    }
+
+    public List<Ability> getAbilities() {
+        return abilities;
+    }
+
+    public int getMaxHP() {
+        return maxHP;
+    }
+
+    public int getCurrentHP() {
+        return currentHP;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCurrentHP(int currentHP) {
+        this.currentHP = currentHP;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public LivingStatus getLivingStatus() {
+        return livingStatus;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public void setLivingStatus(LivingStatus livingStatus) {
+        this.livingStatus = livingStatus;
+    }
+
+    public void levelUp() {
+        level++;
+        maxHP += 5;
+        currentHP = maxHP;
+        attack += 2;
+        System.out.println(name + " leveled up to level " + level + "!");
+    }
+
+    public int attack(Pokemon target) {
+        switch (status) {
+            case CAUGHT -> {
+                System.out.println("What ability would you like to use?");
+                for (int i = 0; i < abilities.size(); i++) {
+                    System.out.println(i + 1 + ". " + abilities.get(i).getName());
+                }
+                Scanner choice = new Scanner(System.in);
+                Ability ability = abilities.get(choice.nextInt() - 1);
+                System.out.println(name + " used " + ability.getName() + " on " + target.getName() + "!");
             }
-            Scanner choice = new Scanner(System.in);
-            Ability ability = abilities[choice.nextInt() - 1];
-            System.out.println(name + " used " + ability.getName() + " on " + target.getName() + "!");
+            case WILD, TAMED ->
+                    System.out.println(name + " used " + abilities.getFirst().getName() + " on " + target.getName() + "!");
         }
+        return attack / 5;
+    }
+    public void faint() {
+        livingStatus = LivingStatus.FAINTED;
+        System.out.println(name + " has fainted!");
+    }
 }
