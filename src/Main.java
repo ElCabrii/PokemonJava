460a9cd9212573c8dabb49904bbc9b4f8b217770
ElCabrii <144121693+ElCabrii@users.noreply.github.com>
2024-06-04 14:11:42 +0200

    temporary main for testing purposes

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,21 @@
 package src;
 
+import org.json.simple.parser.ParseException;
 import src.Chapters.*;
 
-
+import java.io.IOException;
 
 
 public class Main {
-    public static void main(String[] args) {
-
-        Intro.introduction(args);
-
+    public static void main(String[] args) throws IOException, ParseException {
+        Game.readFromJson();
+        //Trainer player = Game.start();
+        Trainer player = new Trainer("Gabriel");
+        player.addPokemonToTeam(Game.getPokemon("Bulbasaur"));
+        Trainer opponent = new Trainer("Opponent");
+        opponent.addPokemonToTeam(Game.getPokemon("Charmander"));
+        System.out.println(player.getName());
+        System.out.println(player.getTeam()[0].getName());
+        //player.fight(opponent);
     }
 }
