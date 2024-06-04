e9fba49d01244eb3f1e565675c031dbbf3e2c442
ElCabrii <144121693+ElCabrii@users.noreply.github.com>
2024-06-04 14:09:00 +0200

    asking name and returning new player

--- src/Chapters/TheAwakening.java
+++ src/Chapters/TheAwakening.java
@@ -1,22 +1,22 @@
 package src.Chapters;
 
-import src.Player;
+import src.Trainer;
 import src.TextDisplayer;
 
 public class TheAwakening {
-    public static void wakeUp(String[] args) {
-
+    public static Trainer wakeUp() {
+        Trainer player = Trainer.askName();
         String textToDisplay =
                 "You start to wake up ...\n" +
-                        "You remerbering the last night \n" +
+                        "You remembering the last night \n" +
                         "The Party Was amazing for your 18 years old \n" +
                         "But you drink a little bit too much \n" +
                         "You look around you and you see a Pokemon \n" +
-                        "A familiar voice is talking your name \n" +
-                        Player.getName() + " ! " +
+                        "A familiar voice is calling your name \n" +
+                        player.getName() + " ! \n" +
                         "Professor Java: I'm glad you wake up ! \n" +
                         "You tried to beat your dad in pokeshot battle last night \n" +
-                        "But you lose and you fall asleep \n" +
+                        "But you lost and you fell asleep \n" +
                         "He carry you in my laboratory, he want you to choose your first Pokemon asap!\n" +
                         "He told me that you will be a great Pokemon Trainer , and he dont want to pay food for you now you are major...\n" +
                         "You need to choose your first Pokemon now !\n" +
@@ -25,9 +25,7 @@ public class TheAwakening {
                         "2. Charmander\n" +
                         "3. Squirtle\n" +
                         "Choose wisely !";
-        //TheChoice.getPokemon(args);
         TextDisplayer.displayParagraph(textToDisplay);
-
-        TheChoice.theChoice(args);
+        return TheChoice.theChoice(player);
     }
 }
