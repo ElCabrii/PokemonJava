bb86341630b0667d0f48125e79cbb22d18dc1b32
ElCabrii <144121693+ElCabrii@users.noreply.github.com>
2024-06-05 23:53:15 +0200

    English grammatical mistakes fixed

--- src/Chapters/TheAwakening.java
+++ src/Chapters/TheAwakening.java
@@ -8,23 +8,23 @@ public class TheAwakening {
         Trainer player = Trainer.askName();
         String textToDisplay =
                 "You start to wake up ...\n" +
-                        "You remembering the last night \n" +
-                        "The Party Was amazing for your 18 years old \n" +
-                        "But you drink a little bit too much \n" +
-                        "You look around you and you see a Pokemon \n" +
-                        "A familiar voice is calling your name \n" +
+                        "You try to remember the last night \n" +
+                        "The party was amazing for your 18 years old \n" +
+                        "But you drank a little bit too much... \n" +
+                        "You take a quick look-around and see a Pokemon \n" +
+                        "A familiar voice calls your name : \n Professor Java: " +
                         player.getName() + " ! \n" +
-                        "Professor Java: I'm glad you wake up ! \n" +
+                        "Professor Java: I'm glad you woke up ! \n" +
                         "You tried to beat your dad in pokeshot battle last night \n" +
-                        "But you lost and you fell asleep \n" +
-                        "He carry you in my laboratory, he want you to choose your first Pokemon asap!\n" +
-                        "He told me that you will be a great Pokemon Trainer , and he dont want to pay food for you now you are major...\n" +
-                        "You need to choose your first Pokemon now !\n" +
-                        "Here are the 3 Pokemon you can choose from : \n" +
+                        "But you lost and fell asleep \n" +
+                        "He carried you in my laboratory, he wanted you to choose your first Pokemon asap!\n" +
+                        "He told me that you will be a great Pokemon Trainer , and he doesnt want to pay for your food as you turned 18...\n" +
+                        "You now need to choose your first Pokemon !\n" +
+                        "Here are the 3 Pokemons you can choose to have : \n" +
                         "1. Bulbasaur\n" +
                         "2. Charmander\n" +
                         "3. Squirtle\n" +
-                        "Choose wisely !";
+                        "Which one will you go for ? Choose wisely !";
         TextDisplayer.displayParagraph(textToDisplay);
         return TheChoice.theChoice(player);
     }
