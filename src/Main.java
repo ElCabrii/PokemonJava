f0d90c6cca861fbff1f430feea547db83ade4a88
ElCabrii <144121693+ElCabrii@users.noreply.github.com>
2024-06-05 23:52:21 +0200

    main back to functionnal story-mode

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ package src;
 
 import org.json.simple.parser.ParseException;
 import src.Chapters.*;
+import src.Pokemon.Status;
 
 import java.io.IOException;
 
@@ -9,13 +10,6 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException, ParseException {
         Game.readFromJson();
-        //Trainer player = Game.start();
-        Trainer player = new Trainer("Gabriel");
-        player.addPokemonToTeam(Game.getPokemon("Bulbasaur"));
-        Trainer opponent = new Trainer("Opponent");
-        opponent.addPokemonToTeam(Game.getPokemon("Charmander"));
-        System.out.println(player.getName());
-        System.out.println(player.getTeam()[0].getName());
-        //player.fight(opponent);
+        Trainer player = Game.start();
     }
 }
