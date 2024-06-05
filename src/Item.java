a0e85453d8f75337b8a72b218e1ab8fcf65c4ace
ElCabrii <144121693+ElCabrii@users.noreply.github.com>
2024-06-05 23:52:09 +0200

    item class filled

--- src/Item.java
+++ src/Item.java
@@ -1,5 +1,20 @@
 package src;
 
 public class Item {
+    private String name;
+    private String description;
+    public Item(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+    public void use() {
+        System.out.println("You used " + name + "!");
+    }
+    public String getName() {
+        return name;
+    }
+    public String getDescription() {
+        return description;
+    }
 
 }
