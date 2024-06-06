24e2b8e81f9b71c626b231c0bc1bef26bdef592f
nvtnicolas <nicolas.nguyenvanthanh@ynov.com>
2024-06-06 18:31:21 +0200

    Continue story

--- src/main/java/com/pokemongame/Chapters/FirstRoad.java
+++ src/main/java/com/pokemongame/Chapters/FirstRoad.java
@@ -1,5 +1,25 @@
 package com.pokemongame.Chapters;
+import com.pokemongame.Chapters.Home;
+import com.pokemongame.TextDisplayer;
+import com.pokemongame.Trainer;
+import com.pokemongame.Game;
 
 public class FirstRoad {
+    public static Trainer firstRoad(Trainer player){
+            String[]textToDisplay=
+                    { "You reach the first road outside the village. \n"
+
+                    };
+
+
+        TextDisplayer.displayParagraph(textToDisplay[0]);
+       //
+        // player.path();
+
+
+
+
+        return player;
+    }
 }
 
--- src/main/java/com/pokemongame/Chapters/Home.java
+++ src/main/java/com/pokemongame/Chapters/Home.java
@@ -25,18 +25,21 @@ public class Home {
                             "I will beat you and become the champion! \n" +
                             name + ": We will see about that! \n" +
                             "Bedrock: Let's battle! \n",
-                            name +"You see, i'm already better! \n"
-
-
-
+                            name +"You see, i'm already better! \n"+
+                            "Bye, see you when you will \n" +
+                            "you start to walk through the village,"+
+                            "When you pass, everyone greets you and wishes you good luck on your trip.\n"+
+                            "You start to feel a bit strange, adrenalin of becoming an adult and travel alone circulate your veins.\n"+
+                            "it's the beginning of your adrventure !!! "
                     };
+
             TextDisplayer.displayParagraph(textToDisplay[0]);
             Trainer bedrock = new Trainer("Bedrock");
             bedrock.addPokemonToTeam(Game.getPokemon("Squirtle", Status.TAMED));
             player.fight(bedrock);
             TextDisplayer.displayParagraph(textToDisplay[1]);
 
-            return player;
+            return FirstRoad.firstRoad(player);
         }
     }
 
