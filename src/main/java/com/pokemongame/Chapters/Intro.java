1f8a40d5b1fcab404d78d1d11532be248e70a4c4
nvtnicolas <nicolas.nguyenvanthanh@ynov.com>
2024-06-16 15:15:58 +0200

    Modify for call the txt with the story and dont have the line in the code

--- src/main/java/com/pokemongame/Chapters/Intro.java
+++ src/main/java/com/pokemongame/Chapters/Intro.java
@@ -5,14 +5,8 @@ import com.pokemongame.Trainer;
 
 public class Intro {
     public static Trainer start() {
-        String textToDisplay =
-                "Welcome in a new world !!!\n" +
-                        "A world full of mysterious creature called : Pokemon !\n" +
-                        "You reached the age required for start your own adventure. \n" +
-                        "In this world, you will meet a lot of people, some of them will be your friends, some of them will be your ennemies.\n" +
-                        "Your goal is to become the best Pokemon Trainer in the world. \n" +
-                        "But before that, you need to choose your first Pokemon \n" +
-                        "Let's go !\n";
+        String textToDisplay = TextDisplayer.readStoryFile("data/story/Intro");
+
         TextDisplayer.printWithDelay(textToDisplay);
 
        return TheAwakening.wakeUp();
--- src/main/java/com/pokemongame/Chapters/TheAwakening.java
+++ src/main/java/com/pokemongame/Chapters/TheAwakening.java
@@ -7,26 +7,11 @@ public class TheAwakening {
     public static Trainer wakeUp() {
         Trainer player = Trainer.askName();
         String[] textToDisplay = {
-                "You start to wake up ...\n" +
-                        "You try to remember the last night \n" +
-                        "The party was amazing for your 18 years old \n" +
-                        "But you drank a little bit too much... \n" +
-                        "You take a quick look-around and see a Pokemon \n" +
-                        "A familiar voice calls your name !\n",
-                        "Professor Java: " + player.getName() + " ! \n" + "Professor Java: I'm glad you woke up ! \n" +
-                        "You tried to beat your dad in pokeshot battle last night \n" +
-                        "But you lost and fell asleep \n" +
-                        "He carried you in my laboratory, he wanted you to choose your first Pokemon asap!\n" +
-                        "He told me that you will be a great Pokemon Trainer , and he doesnt want to pay for your food as you turned 18...\n" +
-                        "You now need to choose your first Pokemon !\n" +
-                        "Here are the 3 Pokemons you can choose to have : \n" +
-                        "1. Bulbasaur\n" +
-                        "2. Charmander\n" +
-                        "3. Squirtle\n" +
-                        "Which one will you go for ? Choose wisely !\n"
+                TextDisplayer.readStoryFile("data/story/TheAwakening1"),
+                TextDisplayer.readStoryFile("data/story/TheAwakening2")
         };
+        textToDisplay[1]=textToDisplay[1].replace("player.getName()", player.getName());
         TextDisplayer.printWithDelay(textToDisplay[0]);
-
         TextDisplayer.printWithDelay(textToDisplay[1]);
         return TheChoice.theChoice(player);
     }
